c793d1d43628b5dd7186444f33064a83f768d3ee
Ann Katrin Kuessner <ann.katrin.kuessner@gmail.com>
2016-11-30 09:32:44 +0100

    Add failing test for #3

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -4,26 +4,42 @@ describe "Author creation page", :type => :feature do
 
   it "should display 'New Author'" do
     visit '/authors/new'
+
     expect(page).to have_text("New Author")
   end
 
   it "should have a 'First Name' input field" do
     visit '/authors/new'
+
     expect(page).to have_text("First name")
   end
 
   it "should have a 'Last Name' input field" do
     visit '/authors/new'
+
     expect(page).to have_text("Last name")
   end
 
   it "should have a 'Homepage' input field" do
     visit '/authors/new'
+
     expect(page).to have_text("Homepage")
   end
 
   it "should have a submit button 'Save Author'" do
     visit '/authors/new'
+
     expect(page.find_button("Save Author").value).to eq("Save Author")
   end
+
+  it "should create new author instances" do
+    visit '/authors/new'
+    fill_in('First name', with: 'Alan')
+    fill_in('Last name', with: 'Turing')
+    fill_in('Homepage', with: "http://wikipedia.org/Alan_Turing")
+    click_button("Save Author")
+
+    expect(Author.where(first_name:'Alan', last_name: 'Turing', homepage: "http://wikipedia.org/Alan_Turing")).to exist
+  end
+
 end
