fcbeafeaa0aafdc8f076f8394650874c91ed9ea7
Ann Katrin Kuessner <ann.katrin.kuessner@gmail.com>
2016-12-18 17:01:49 +0100

    ...

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -5,10 +5,8 @@ describe "Author index page", :type => :feature do
 
   it"should link to the new author page" do
     visit authors_path
-
     expect(page).to have_css("a", text:"add author")
   end
-
 
 end
 
