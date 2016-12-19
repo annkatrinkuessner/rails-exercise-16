c67281b019d0908600c4e25e935efc83351eba5e
Ann Katrin Kuessner <ann.katrin.kuessner@gmail.com>
2016-12-19 17:34:11 +0100

    Closes #11

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -15,12 +15,11 @@ class AuthorsController < ApplicationController
   end
 
   def edit
+    @author = Author.find(params[:id])
   end
 
   def create
     @author = Author.new(author_params)
-
-
     if @author.save
       redirect_to @author
     else
@@ -29,10 +28,16 @@ class AuthorsController < ApplicationController
   end
 
   def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to @author
+    else
+      render 'edit'
+    end
   end
 
   def destroy
-
+    @author = Author.find(params[:id])
   end
 
 
