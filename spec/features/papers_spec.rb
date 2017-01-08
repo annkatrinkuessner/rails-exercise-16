ac60ee16fd5f247ddeb3ec5ed42166113ee29bea
Ann Katrin Kuessner <ann.katrin.kuessner@gmail.com>
2017-01-08 18:30:49 +0100

    Failig test

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -10,4 +10,10 @@ describe "New  paper page", :type => :feature do
     expect(page).to have_text("Year")
   end
 
+  it "should have a submit button" do
+    visit new_paper_path
+    expect(page).to have_css("input[type='submit']")
+
+  end
+
 end
