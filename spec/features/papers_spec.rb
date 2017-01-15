f9dfa1053b73047de0ccd0aaeed60bea6084a340
Ann Katrin Kuessner <ann.katrin.kuessner@gmail.com>
2017-01-15 12:09:33 +0100

    Faiilng test for #21 and edit papers

--- spec/features/papers_spec.rb
+++ spec/features/papers_spec.rb
@@ -15,6 +15,11 @@ describe "New paper page", :type => :feature do
     expect(page).to have_css("input[type='submit']")
   end
 
+  it "should link to index page" do
+    visit new_paper_path
+    expect(page).to have_css("a", text: "Back")
+  end
+
   it "should save the paper" do
     visit new_paper_path
     fill_in('Title', with: 'COMPUTING MACHINERY AND INTELLIGENCE')
@@ -59,4 +64,49 @@ describe "Paper index page", :type => :feature do
   expect(page).to have_text("1950")
   expect(page).to have_text("Mind 49: 433-460")
   end
+
+  it "should link to the new paper page" do
+    visit papers_path
+    expect(page).to have_css("a", text:"add paper")
+  end
+
+end
+
+describe "Edit paper page", :type => :feature do
+
+  it "should render" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+  end
+
+  it "should link to the paper index page" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+    expect(page).to have_css("a", text:"Back")
+  end
+
+  it "should update year" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+    fill_in("Year", with: "2017")
+    click_button("Submit")
+    expect(page).to have_text("2017")
+  end
+
+  it "should update venue" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+    fill_in("Venue", with: "UIST")
+    click_button("Submit")
+    expect(page).to have_text("UIST")
+  end
+
+  it "should update title" do
+    paper = create(:paper)
+    visit edit_paper_path(paper)
+    fill_in("Title", with: "Awesome Paper")
+    click_button("Submit")
+    expect(page).to have_text("Awesome Paper")
+  end
+
 end
